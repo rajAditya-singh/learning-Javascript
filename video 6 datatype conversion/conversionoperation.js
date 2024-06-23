2cd1ad59acaf912d127f65a239f7d308c618a9b7
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2024-06-23 11:41:10 +0530

    update

---
+++ video 6 datatype conversion/conversionoperation.js
@@ -0,0 +1,46 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "raj"
+
+// console.log(score);
+// console.log(typeof score);
+
+// let ValueInNumber = Number(score)
+// console.log(ValueInNumber);
+// console.log(typeof ValueInNumber);
+
+/* 
+"33" = 33
+"33abc" = NaN
+null = 0
+undefined = NaN
+true = 1, false = 0
+"raj" = NaN
+*/
+
+// let login = "raj"
+// console.log(login);
+// console.log(typeof login);
+// let ValueInBoolean = Boolean(login)
+// console.log(ValueInBoolean);
+// console.log(typeof ValueInBoolean);
+
+/*
+1 = true
+0 = false
+null = false
+undefined = false
+"" = false
+"raj" = true
+*/
+
+let num = true
+console.log(num);
+console.log(typeof num);
+
+let ValueInString = String(num)
+console.log(ValueInString);
+console.log(typeof ValueInString);
