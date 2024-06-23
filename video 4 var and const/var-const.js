fb71f28364d7dacd2fbe6509236962fe4cf45b12
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2024-06-23 11:09:26 +0530

    update

---
+++ video 4 var and const/var-const.js
@@ -0,0 +1,14 @@
+const accountId = 12345;
+let accountEmail = "hdfc@gmail.com"
+var accountPassword = "123"
+accountCity = "UP"
+let accountState
+
+// changed details
+// accountId = 111111 not allowed
+accountEmail = "BOB@gmail.com"
+accountPassword = "adi"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ video 5 datatypes/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treate all js code as newer version
+
+// alert(3+3) we are using node js not browser. In browser console it will work.
+// code readiability should be high
+
+let name = "raj"
+let age = 23
+let placed = false
+let temp = null
+let state;
+
+/* Datatypes
+-----Primitive datatypes-----
+number = range to 2^53
+bigint
+string = ""
+boolean = true and false
+null = stand alone value
+undefined
+symbol = unique
+*/
+
+console.log(typeof age);
+console.log(typeof "raj");
+console.log(typeof placed);
+console.log(typeof temp);
+console.log(typeof state);
+// -----object----
+console.log(typeof null); // type-object
+console.log(typeof undefined); //undefined
+
+
+
