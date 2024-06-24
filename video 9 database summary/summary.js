afcb33223df9914b3f86d8a41fe5f32c08d9bd2f
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2024-06-25 00:04:38 +0530

    Create summary.js

---
+++ video 9 database summary/summary.js
@@ -0,0 +1,41 @@
+// -------primitive datatype--------
+/*
+string
+number
+boolean
+null
+undefined
+symbol
+bigint
+*/
+
+const score = 100
+const sconeval = 100.3
+const login = false
+const temp = null
+let email;
+
+const id = Symbol("123")
+const newid = Symbol("123")
+console.log(id === newid);
+
+const bigint = 123456789645453453453453n
+
+
+// ------reference datatype or non permitive------
+
+// array
+const hero = ["shaktiman", "marvel", "ghoda"]
+// object
+let myobject = {
+    name: "raj",
+    age: 23,
+    mail: "study@gmail",
+}
+// function
+const myfunction = function(){
+    console.log("raj");
+}
+
+
+
