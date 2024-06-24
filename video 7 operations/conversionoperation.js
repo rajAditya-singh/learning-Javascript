10969ed646f11a58174a4bf6bedecc08253d8fb8
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2024-06-24 12:58:44 +0530

    update

---
+++ video 7 operations/conversionoperation.js
@@ -0,0 +1,72 @@
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = "raj"
+
+// console.log(score);
+// console.log(typeof score);
+
+// let ValueInNumber = Number(score)
+// console.log(ValueInNumber);
+// console.log(typeof ValueInNumber);
+
+/* 
+"33" = 33
+"33abc" = NaN
+null = 0
+undefined = NaN
+true = 1, false = 0
+"raj" = NaN
+*/
+
+// let login = "raj"
+// console.log(login);
+// console.log(typeof login);
+// let ValueInBoolean = Boolean(login)
+// console.log(ValueInBoolean);
+// console.log(typeof ValueInBoolean);
+
+/*
+1 = true
+0 = false
+null = false
+undefined = false
+"" = false
+"raj" = true
+*/
+
+// let num = true
+// console.log(num);
+// console.log(typeof num);
+
+// let ValueInString = String(num)
+// console.log(ValueInString);
+// console.log(typeof ValueInString);
+
+// -------------------operations------------------//
+// let value = 3
+// let negVal = -value
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+// let str1 = "Hello"
+// let str2 = " raj"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
