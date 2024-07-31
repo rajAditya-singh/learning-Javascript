4167a9657db52130d30361779e4809d46c1be9f7
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2024-07-31 22:28:59 +0530

    s

---
+++ video 26 control flow/truthy.js
@@ -0,0 +1,37 @@
+// const userEmail = []
+// if (userEmail) {
+//     console.log("Got user email");
+// }
+// else{
+//     console.log("Don't have user email");
+// }
+
+//falsy values = false, 0, -0, bigint 0n, "", null, undefined, NaN.
+
+//Truthy values = "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+// const empObj = {}
+// if (Object.keys(empObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
+//Nullish Coalescing Operator (??) : null undefined
+// let val1;
+
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? 10 ?? 100
+// console.log(val1);
+
+// Terniary operator
+// syntax = condition ? true:false
+
+const iceteaprice = 100; 
+iceteaprice <= 80 ? console.log("Less than 100") : console.log("More than 100");
+
+
