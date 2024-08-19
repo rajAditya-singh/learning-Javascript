21750751cce76c399599405f92f3e59b217f0942
Aditya Raj Singh <167435837+rajAditya-singh@users.noreply.github.com>
2024-08-20 00:19:04 +0530

    Create adv.js

---
+++ video_29/adv.js
@@ -0,0 +1,30 @@
+//FOR OF LOOP
+
+//["", "", ""]
+//[{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5, 6];
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "HELLO WORLD";
+for (const greet of greetings) {
+    // console.log(`Each Char is ${greet}`);
+    
+}
+
+
+//MAP 
+//known for unique values(no repetions), Correct order
+
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('VK', "Virat Kohli")
+map.set('MSD', "Ms Dhoni")
+map.set('IN', "India")
+
+console.log(map);
+
+
+
